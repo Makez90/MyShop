5bebf9abdd3f1079c1cded20724fea3fea7c520c
Makez90 <44731217+Makez90@users.noreply.github.com>
2020-03-28 18:24:27 +0100

    Basket Testing

---
+++ MyShop/MyShop.WebUI.Tests/Mocks/MockHttpContext.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MyShop.WebUI.Tests.Mocks
+{
+    public class MockHttpContext : HttpContextBase
+    {
+        private MockResquest request;
+        private MockResponse response;
+        private HttpCookieCollection cookies;
+
+        public MockHttpContext()
+        {
+            cookies = new HttpCookieCollection();
+            this.request = new MockResquest(cookies);
+            this.response = new MockResponse(cookies);
+        }
+
+        public override HttpRequestBase Request
+        {
+            get
+            {
+                return request;
+            }
+        }
+
+        public override HttpResponseBase Response
+        { 
+            get
+            {
+                return response;
+            }
+        }
+    }
+
+    public class MockResponse : HttpResponseBase
+    {
+        private readonly HttpCookieCollection cookies;
+
+        public MockResponse(HttpCookieCollection cookies)
+        {
+            this.cookies = cookies;
+        }
+        public override HttpCookieCollection Cookies
+        {
+            get
+            {
+                return cookies;
+            }
+        }
+    }
+
+    public class MockResquest : HttpRequestBase
+    {
+        private readonly HttpCookieCollection cookies;
+
+        public MockResquest(HttpCookieCollection cookies)
+        {
+            this.cookies = cookies;
+        }
+        public override HttpCookieCollection Cookies
+        {
+            get
+            {
+                return cookies;
+            }
+        }
+    }
+
+}
